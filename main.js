c84d655a4e97a086ab5441795dea5fbc25881d20
大嶋由真 <>
2022-07-01 02:52:35 +0900

    修正

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ let todos = [];
 
 // 追加ボタンがクリックされたら実行する処理を実装
 submitButton.addEventListener('click', () => {
-<<<<<<< HEAD
   const taskStatus = '作業中';
   todos.push({
     id: todos.length,
@@ -49,26 +48,3 @@ const createListView = () => {
     todoLists.appendChild(todoItem);
   });
 };
-=======
-  const pushTodo = addTodo();
-  todos.push(pushTodo);
-  showTodos();
-  // pushされた後に空の文字列を値に代入することで、タスク登録後インプットの中身が空になり、再び新しく登録できるようになる
-  addTask.value = '';
-});
-
-// todosの中身を一覧表示させる
-const showTodos = () => {
-  
-  todos.forEach((todo) => {
-    const todoItem = document.createElement('tr');
-    todoLists.appendChild(todoItem);
-    const todoIdNam = document.createElement('th');
-    const todoStatus = document.createElement('th');
-    const todoAction = document.createElement('th');
-// tr,tdタグを作成した後、innerHTMLでID、コメント、状態、削除ボタンを出力する
-    todoIdNam.innerHTML = todo.idNum;
-
-  });
-}
->>>>>>> d71b8c13752315783365523a8187ccf56709eafa
