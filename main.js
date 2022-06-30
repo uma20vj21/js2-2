b43ca49ec5f90f22c367aee2c2d8a1a0375cfff9
大嶋由真 <>
2022-07-01 02:43:32 +0900

    2-2-1完了

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
+'use strict';
 // HTMLより入力欄、追加ボタン、追加した内容を表示するためにIDを取得
 const addTask = document.getElementById('addtask');
 const submitButton = document.getElementById('submit');
 const todoLists = document.getElementById('todolists');
 
 // 追加するための空配列を定義
-const todos = [];
-
-// 出力の際のID定義
-const todoId = 0;
-
-// todosに格納するためのtodoを定義
-const addTodo = () => {
-  const idNum = todoId;
-  const task = addTask.value;
-  const status = '作業中';
-  const todo = {
-    idNum,
-    task,
-    status,
-  };
-  //  todoを返す
-  // return todo;
-};
+let todos = [];
 
 // 追加ボタンがクリックされたら実行する処理を実装
 submitButton.addEventListener('click', () => {
-  const pushTodo = addTodo();
-  todos.push(pushTodo);
+  const taskStatus = '作業中';
+  todos.push({
+    id: todos.length,
+    comment: addTask.value,
+    status: taskStatus,
+  });
+
+  createListView();
+
   addTask.value = '';
 });
 
-// tososの中身を一覧表示させる
-const showTodos = (selecttodos) => {
-  todolists = '';
-  
-  const table = document.createElement('tr');
-  
-}
+const createListView = () => {
+  // todosLists直下に子要素がなくなるまで、子要素を削除し続ける処理
+  while (todoLists.firstChild) {
+    todoLists.removeChild(todoLists.firstChild);
+  }
+  todos.forEach((task) => {
+    let todoItem = document.createElement('tr');
+    const todoId = document.createElement('th');
+    const todoComment = document.createElement('th');
+    const todoStatus = document.createElement('th');
+    const todoDelete = document.createElement('th');
+    const deleteButton = document.createElement('button');
+
+    todoId.innerHTML = task.id;  //コールバック関数の第一引数(task)からtodosの各値を取得させinnerHTMLで記述
+    todoComment.innerHTML = task.comment;  //コールバック関数の第一引数(task)からtodosの各値を取得させinnerHTMLで記述
+    deleteButton.innerHTML = '削除';
+    deleteButton.classList.add('delete');  //要素に'delete'クラスを付与
+    todoDelete.appendChild(deleteButton); //上記で新しく'delete'クラスをつくりth要素に差し込む
+
+    todoItem.appendChild(todoId);
+    todoItem.appendChild(todoComment);
+    todoItem.appendChild(todoStatus);
+    todoItem.appendChild(todoDelete);
+
+    todoLists.appendChild(todoItem);
+  });
+};
