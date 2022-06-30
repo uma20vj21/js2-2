824b9f9105f039e5ebd15af96703b7c1906b188e
大嶋由真 <>
2022-06-30 09:56:07 +0900

    作業途中

--- main.js
+++ main.js
@@ -1,2 +1,39 @@
+// HTMLより入力欄、追加ボタン、追加した内容を表示するためにIDを取得
+const addTask = document.getElementById('addtask');
+const submitButton = document.getElementById('submit');
+const todoLists = document.getElementById('todolists');
 
+// 追加するための空配列を定義
+const todos = [];
 
+// 出力の際のID定義
+const todoId = 0;
+
+// todosに格納するためのtodoを定義
+const addTodo = () => {
+  const idNum = todoId;
+  const task = addTask.value;
+  const status = '作業中';
+  const todo = {
+    idNum,
+    task,
+    status,
+  };
+  //  todoを返す
+  // return todo;
+};
+
+// 追加ボタンがクリックされたら実行する処理を実装
+submitButton.addEventListener('click', () => {
+  const pushTodo = addTodo();
+  todos.push(pushTodo);
+  addTask.value = '';
+});
+
+// tososの中身を一覧表示させる
+const showTodos = (selecttodos) => {
+  todolists = '';
+  
+  const table = document.createElement('tr');
+  
+}
