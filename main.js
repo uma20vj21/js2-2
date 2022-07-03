6cd4b6949d5ad86c44da791c556c47674cc44406
uma20vj21 <98353128+uma20vj21@users.noreply.github.com>
2022-07-03 20:00:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -19,26 +19,32 @@ submitButton.addEventListener('click', () => {
 
   addTask.value = '';
 });
+// 削除ボタンが押されたらタスクが削除される処理を実行
+const deleteButton = (taskid) =>{
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '削除';
+  deleteBtn.addEventListener('click',() => {
+    todos.slice(taskid, 1);
+    createListView(todos);
+ };
+};
 
-const createListView = () => {
+const createListView = (todos) => {
   // todosLists直下に子要素がなくなるまで、子要素を削除し続ける処理
   while (todoLists.firstChild) {
     todoLists.removeChild(todoLists.firstChild);
   }
-  todos.forEach((task) => {
+  todos.forEach((task, index) => {
     const todoItem = document.createElement('tr');
     const todoId = document.createElement('th');
     const todoComment = document.createElement('th');
     const todoStatus = document.createElement('th');
     const todoDelete = document.createElement('th');
-    const deleteButton = document.createElement('button');
-
-    todoId.innerHTML = task.id; //コールバック関数の第一引数(task)からtodosの各値を取得させinnerHTMLで記述
+    
+    todoId.innerHTML = index + 1; //コールバック関数のインデックスからIDを取得させinnerHTMLで記述
     todoComment.innerHTML = task.comment; //コールバック関数の第一引数(task)からtodosの各値を取得させinnerHTMLで記述
     todoStatus.innerHTML = task.status;
-    deleteButton.innerHTML = '削除';
-    deleteButton.classList.add('delete'); //要素に'delete'クラスを付与
-    todoDelete.appendChild(deleteButton); //上記で新しく'delete'クラスをつくりth要素に差し込む
+    todoDelete.appendChild(deleteButton(index)); //インデックスの値を与えてあげる(taskid)の仮引数にtodos配列のインデックスをいれるイメ―ジ
 
     todoItem.appendChild(todoId);
     todoItem.appendChild(todoComment);
